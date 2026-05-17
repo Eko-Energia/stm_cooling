6a0dff8de86c6ba7031183d2d602f4b349986c33
unknown <kkupis@student.agh.edu.pl>
2026-05-18 00:08:36 +0200

    Initial commit - STM32 project

---
+++ Cooling_left/stm_f303k8_ioc/Main_app/Inc/cooler_left_app.h
@@ -0,0 +1,17 @@
+/*
+ * cooler_left_app.h
+ *
+ *  Created on: 8 kwi 2026
+ *      Author: Karol
+ */
+
+#ifndef INC_COOLER_LEFT_APP_H_
+#define INC_COOLER_LEFT_APP_H_
+
+
+void Init_Cooler();
+
+void COOLER_LEFT_app();
+
+
+#endif /* INC_COOLER_LEFT_APP_H_ */
